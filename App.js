904da871feb7661d553afb9f266d7a8c52d91b9a
중현 <jhlcoder@gmail.com>
2021-12-30 16:33:06 +0900

    2021.12.30

---
+++ App.js
@@ -0,0 +1,21 @@
+import Layout from './src/components/commons/layout';
+
+
+function App() {
+  return (
+    <>
+      {/* <Layout> */}
+        <div >
+          qqwe
+        </div>
+      {/* </Layout> */}
+    </>
+  );
+}
+
+export default App;
+
+// useEffect(() => {
+//   document.getElementById('mIframeMobile').scrollIntoView({ behavior: 'smooth' });
+// },[]);
+// ID값으로 스크롤 이동하기
---
+++ src/components/commons/layout/index.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+export default function Layout () {
+    return (
+        <>
+            <div>
+                레이아웃페이지입니다.
+            </div>
+        </>
+    )
+}
